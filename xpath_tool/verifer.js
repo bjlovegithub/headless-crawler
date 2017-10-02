25d44930163490fae58d33f86a32f16b1c49c20b
billjeff <billjeff@yeah.net>
2017-10-02 21:44:47 +1100

    Add tool to extract xpath from dom element.

--- xpath_tool/verifer.js
+++ xpath_tool/verifer.js
@@ -1,5 +1,19 @@
-var puppeteer = require('puppeteer');
+const express = require('express');
+const http = require('http');
 
+const app = express();
+app.use(express.static(__dirname))
+
+app.get('/index.html', function (req, res) {
+  res.sendFile('index.html', options);
+});
+
+const server = http.createServer(app);
+server.listen(8080, function listening() {
+  console.log('Listening on %d', server.address().port);
+});
+
+/*
 (async () => {
 		const browser = await puppeteer.launch();
 		
@@ -10,3 +24,4 @@ var puppeteer = require('puppeteer');
 		const page = await browser.newPage();
 		await page.goto(msg.content.toString());
 })();
+*/
