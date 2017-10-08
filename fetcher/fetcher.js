dd7dfefd436b7b31ab1286f1a9eff2f3a9faca3a
billjeff <billjeff@yeah.net>
2017-10-08 22:42:59 +1100

    Add verifier tool

--- fetcher/fetcher.js
+++ fetcher/fetcher.js
@@ -4,23 +4,23 @@ var puppeteer = require('puppeteer');
 const amqp = require('amqplib/callback_api');
 
 (async () => {
-		const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
+  
+  amqp.connect('amqp://localhost', function(err, conn) {
+	conn.createChannel(function(err, ch) {
+	  var q = 'url';
+
+	  ch.assertQueue(q, {durable: true});
+	  console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+	  ch.consume(q, async (msg) => {
+		console.log(" [x] Received %s", msg.content.toString());
+
+		const page = await browser.newPage();
+		await page.goto(msg.content.toString());
+
 		
-		amqp.connect('amqp://localhost', function(err, conn) {
-				conn.createChannel(function(err, ch) {
-						var q = 'url';
-
-						ch.assertQueue(q, {durable: true});
-						console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-						ch.consume(q, async (msg) => {
-								console.log(" [x] Received %s", msg.content.toString());
-
-								const page = await browser.newPage();
-								await page.goto(msg.content.toString());
-
-								
-								
-						}, {noAck: true});
-				});
-		});
+		
+	  }, {noAck: true});
+	});
+  });
 })();
--- xpath_tool/verifer.js
+++ xpath_tool/verifer.js
@@ -1,27 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const http = require('http');
+const puppeteer = require('puppeteer-xpath');
 
 const app = express();
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
 app.get('/index.html', function (req, res) {
   res.sendFile('index.html', options);
 });
+
+const browser = (async () => {
+  return await puppeteer.launch()
+})()
 
 const server = http.createServer(app);
 server.listen(8080, function listening() {
   console.log('Listening on %d', server.address().port);
 });
 
-/*
-(async () => {
-		const browser = await puppeteer.launch();
-		
-		var q = 'url';
+app.post('/verify', function (req, res) {
+  const val = (async (url, xpath) => {
+	const page = await browser.newPage();
+	await page.goto(url);
+	await page.waitForXpath(xpath)  
+	val = await page.$XPath(xpath)
+	return val
+  })(req.body.url, req.body.xpath);
 
-		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+  res.send(val)
+})		
 
-		const page = await browser.newPage();
-		await page.goto(msg.content.toString());
-})();
-*/
