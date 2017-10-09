10e01f5077b8af32ec4fd61561452502671831a1
billjeff <billjeff@yeah.net>
2017-10-09 22:42:32 +1100

    Fix browser not being initialized error

--- xpath_tool/verifer.js
+++ xpath_tool/verifer.js
@@ -14,24 +14,27 @@ app.get('/index.html', function (req, res) {
   res.sendFile('index.html', options);
 });
 
-const browser = (async () => {
-  return await puppeteer.launch()
+(async () => {
+  var browser = await puppeteer.launch()
+
+  app.post('/verify', function (req, res) {
+	(async () => {
+	  const page = await browser.newPage();
+	  console.log(req)
+	  await page.goto(req.body.url);
+	  await page.waitForXpath(req.body.xpath)  
+	  val = await page.$XPath(req.body.xpath)
+
+	  res.send(val)
+	})()
+  })		
+
+  const server = http.createServer(app);
+  server.listen(8080, function listening() {
+	console.log('Listening on %d', server.address().port);
+  });
+  
 })()
 
-const server = http.createServer(app);
-server.listen(8080, function listening() {
-  console.log('Listening on %d', server.address().port);
-});
 
-app.post('/verify', function (req, res) {
-  const val = (async (url, xpath) => {
-	const page = await browser.newPage();
-	await page.goto(url);
-	await page.waitForXpath(xpath)  
-	val = await page.$XPath(xpath)
-	return val
-  })(req.body.url, req.body.xpath);
-
-  res.send(val)
-})		
 
